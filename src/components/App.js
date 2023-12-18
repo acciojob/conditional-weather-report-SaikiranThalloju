8525a757d5fb811d1922de1a6d577c95d89a58b1
Saikiran Thalloju <saikiranthalloju008@gmail.com>
2023-12-18 19:23:20 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,21 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const weatherData = { temperature: 25, conditions: "Sunny" };
+  const temperatureThreshold = 20;
+
+  const temperatureColor = weatherData.temperature > temperatureThreshold ? 'red' : 'blue';
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Weather App</h1>
+      <div style={{ color: temperatureColor }}>
+        <p>Temperature: {weatherData.temperature}°C</p>
+        <p>Conditions: {weatherData.conditions}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
