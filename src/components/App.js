a0604c3b9a25a4fd890b930f0476b9026c7255f8
Saikiran Thalloju <saikiranthalloju008@gmail.com>
2023-12-29 17:00:14 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  const [weatherInput, setWeatherInput] = useState({ temperature: 25, conditions: "Sunny" });
-  const [textColor, setTextColor] = useState("blue");
+import React ,{useState, useEffect} from 'react';
 
+const App = () => {
+  const [weatherInput, setWeatherInput] =useState({ temperature: 25, conditions: "Sunny" });
+  const [textColor, setTextColor] = useState("blue");
   useEffect(() => {
     // Set text color based on temperature threshold
     if (weatherInput.temperature > 20) {
@@ -13,24 +14,17 @@ const App = () => {
       setTextColor("blue");
     }
   }, [weatherInput.temperature]);
-
-  const weather = { temperature: 25, conditions: "Sunny" };
-  const temperatureColor = weather.temperature > 20 ? 'red' : 'blue';
-
+ 
   return (
     <div>
-      {/* Do not remove the main div */}
-      <div style={{ color: textColor }}>
+        {/* Do not remove the main div */}
+       <div style ={{color:textColor}}>
         <p>Temperature: {weatherInput.temperature}</p>
         <p>Conditions: {weatherInput.conditions}</p>
-      </div>
-      <div id="main">
-        <h2>Weather Report</h2>
-        <p>Temperature: <span style={{ color: temperatureColor }}>{weather.temperature}</span></p>
-        <p>Conditions: {weather.conditions}</p>
-      </div>
+       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+
+export default App
